b13c5877dffcdcd41b4c53e073f2a449ef0c4390
amulyaAmmu <40600002+amulyaAmmu@users.noreply.github.com>
2024-05-09 14:42:43 -0700

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Amulya Gangam on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
